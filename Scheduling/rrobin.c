ca11b8ce3f1223643c3bc2364822437042fb8afb
Vanshika Jain <83728763+vj-vanshika@users.noreply.github.com>
2022-12-13 05:26:35 +0530

    Update rrobin.c

--- Scheduling/rrobin.c
+++ Scheduling/rrobin.c
@@ -1,67 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-#include <stdio.h>
-#include <conio.h>
-
-void main()
+struct process
 {
-    int i, NOP, sum = 0, count = 0, y, quant, wt = 0, tat = 0, at[10], bt[10], temp[10];
-    float avg_wt, avg_tat;
-    printf(" Total number of process in the system: ");
-    scanf("%d", &NOP);
-    y = NOP;
+    int pid, arr_time, burst_time, temp_b_time;
+    int wt, tat;
+};
+void queue_update(int n, vector<process> p, int time, deque<int> &q);
+int main()
+{
+    int n;
+    cin>>n;
 
-    for (i = 0; i < NOP; i++)
+    vector<process> p(n);
+
+    for(int i=0; i<n;i++)
     {
-        printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i + 1);
-        printf(" Arrival time is: \t");
-        scanf("%d", &at[i]);
-        printf(" \nBurst time is: \t");
-        scanf("%d", &bt[i]);
-        temp[i] = bt[i];
+        cin>>p[i].arr_time>>p[i].burst_time;
+        p[i].pid = i+1;
+        p[i].temp_b_time = p[i].burst_time;
+        
     }
+    int tq;
+    cin>>tq;
+    deque<int> q;
+    int time=0;
+    int count = 0;
+    while(p[0].arr_time>time)
+    time++;
+    while(count<n)
+    {   
+        queue_update(n,p,time,q);
+        
+        int id = q.front();
+        for(int j=1; j<=tq; j++)
+        {   
+            // cout<<"j = "<<j<<endl;
+            time++;
+            queue_update(n,p,time,q);
+            p[id].temp_b_time--;
+            if(p[id].temp_b_time==0)
+            {   
+                count++;
+                p[id].wt = time-p[id].arr_time-p[id].burst_time;
+                p[id].tat = time - p[id].arr_time;
 
-    printf("Enter the Time Quantum for the process: \t");
-    scanf("%d", &quant);
-
-    printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");
-    for (sum = 0, i = 0; y != 0;)
-    {
-        if (temp[i] <= quant && temp[i] > 0)
-        {
-            sum = sum + temp[i];
-            temp[i] = 0;
-            count = 1;
+                cout<<p[id].pid<<" "<<p[id].wt<<" "<<p[id].tat<<endl;
+                break;
+            }
         }
-        else if (temp[i] > 0)
-        {
-            temp[i] = temp[i] - quant;
-            sum = sum + quant;
-        }
-        if (temp[i] == 0 && count == 1)
-        {
-            y--;
-            printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
-            wt = wt + sum - at[i] - bt[i];
-            tat = tat + sum - at[i];
-            count = 0;
-        }
-        if (i == NOP - 1)
-        {
-            i = 0;
-        }
-        else if (at[i + 1] <= sum)
-        {
-            i++;
-        }
-        else
-        {
-            i = 0;
-        }
+        q.pop_front();
     }
+}
 
-    avg_wt = wt * 1.0 / NOP;
-    avg_tat = tat * 1.0 / NOP;
-    printf("\n Average Turn Around Time: \t%f", avg_wt);
-    printf("\n Average Waiting Time: \t%f", avg_tat);
-    getch();
+void queue_update(int n, vector<process> p, int time, deque<int> &q)
+{
+    for(int j=0; j<n;j++)
+        {
+            if(p[j].arr_time<=time && p[j].temp_b_time>0)
+            {  
+                //  cout<<"process is "<<p[j].pid<<endl;
+                auto itr = find(q.begin(), q.end(), j);
+                if(itr == q.end())
+                    q.push_back(j);
+                
+            }
+        }
 }
